0d0ce0ec72a7ea4781968d0f32aa9ef783ac387a
LuisBellocs <luis_bello@ciencias.unam.mx>
2024-10-09 19:04:49 -0600

    nuevo proyecto django

--- cienciastop/cienciastop/wsgi.py
+++ cienciastop/cienciastop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cienciastop project.
+WSGI config for CienciasTop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cienciastop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CienciasTop.settings')
 
 application = get_wsgi_application()
