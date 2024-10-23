a0493684630a62413e91475faa554e75d6813f74
McTavo <mar_padilla@ciencias.unam.mx>
2024-10-23 17:29:23 -0600

    Add/Producto y Usuario, Add/templates, Add/manage comands

--- producto/urls.py
+++ producto/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from . import views
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+
 urlpatterns =[
-    path('agregar/',views.agregar_producto, name='agregar'),
-    path('editar/<int:id>/',views.edita_producto, name='editar')
-]
+    #path('', views.inicio, name='inicio'),
+    path('', views.productos, name='productos'),
+    path('adminproductos', views.admin_producto, name='admin_productos'),
+    path('adminproductos/crear', views.agregar_producto, name='crear'),
+    path('adminproductos/editar', views.agregar_producto, name='editar'),
+    path('eliminar/<int:id>', views.eliminar_producto, name='eliminar'),
+    path('adminproductos/editar/<int:id>', views.editar_producto, name='editar'),
+    path('adminproductos/rentar/<int:id>', views.rentar_producto, name='rentar'),
+    
+    #path('agregar/',views.agregar_producto, name='agregar'),
+    #path('editar/<int:id>/',views.edita_producto, name='editar')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
