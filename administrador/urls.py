7ce168e326872af458815324d1b8f837a0ed5c4c
Aruroo <jonathan.arturo.preciado@ciencias.unam.mx>
2024-11-11 17:29:52 -0600

    eliminando apps inutiles

--- administrador/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin-usuarios/', views.admin_usuarios, name='admin_usuarios'),
-    path('admin-productos/', views.admin_productos, name='admin_productos'),
-    path('permission-denied/', views.permission_denied, name='permission_denied'),
-]
